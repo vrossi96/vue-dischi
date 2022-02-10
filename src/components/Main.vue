18bf349dcf287306dcf0d1fd8aa8fd6fda851a21
vrossi96 <vrossi396@yahoo.it>
2022-02-10 16:48:20 +0100

    added all cards

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,16 @@
 <template>
    <div class="container">
-      <div class="row">
-         <div class="card text-center">
-            <img :src="artists[0].poster" :alt="artists[0].author" />
-            <p class="song-name text-white text-uppercase">{{ artists[0].title }}</p>
-            <div class="auth-year">
-               <p>{{ artists[0].author }}</p>
-               <p>{{ artists[0].year }}</p>
+      <div class="row g-3">
+         <div v-for="artist in artists" :key="artist.author" class="card text-center">
+            <div class="img-cont">
+               <img class="dsa img-fluid" :src="artist.poster" :alt="artist.author" />
+            </div>
+            <div class="card-text d-flex flex-column justify-content-between flex-grow-1">
+               <p class="song-name text-white text-uppercase">{{ artist.title }}</p>
+               <div class="auth-year">
+                  <p>{{ artist.author }}</p>
+                  <p>{{ artist.year }}</p>
+               </div>
             </div>
          </div>
       </div>
@@ -22,11 +26,19 @@ export default {
 
 <style scoped lang="scss">
 .card {
-   width: calc(100% / 5);
-   height: 400px;
-   padding: 15px;
+   width: calc(100% / 5 - 30px);
+   padding: 10px;
    background-color: #2e3a46;
    border: none;
+   margin: 15px 15px;
+
+   .img-cont {
+      height: 60%;
+      img {
+         height: 100%;
+         max-width: 100%;
+      }
+   }
 
    p {
       margin-bottom: 0;
