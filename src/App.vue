4b2d34ff930495a35c5566107f067e64953cb488
vrossi96 <vrossi396@yahoo.it>
2022-02-10 15:54:51 +0100

    added axios and artists array

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
-   <Header />
+   <div>
+      <Header />
+      <Main />
+   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
    name: "App",
    components: {
       Header,
+      Main,
+   },
+   data() {
+      return {
+         artists: [],
+      };
+   },
+   methods: {
+      getArtists() {
+         axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+            this.artists = res.data.response;
+         });
+      },
+   },
+   mounted() {
+      this.getArtists();
    },
 };
 </script>
